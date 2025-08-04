e4389c0d56d4a7a4a83c9070c944ced16c9a1e3e
Tanaka9612 <u15057420@tuks.co.za>
2025-08-04 16:27:35 +0200

    Final changes
    changes to problem files

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-      reactDevOverlay: false, // Disables the ReactDevOverlay
+      // reactDevOverlay: false, // Disables the ReactDevOverlay
     };
 
 export default nextConfig;
