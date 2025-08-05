4912f1630c010f0546a73e9f0cb8fc64e9389f32
Tanaka9612 <u15057420@tuks.co.za>
2025-08-05 12:34:38 +0200

    about us change
    removed normalize-css import

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,5 +1,5 @@
 "use client";
-import 'normalize-css';
+// import 'normalize-css';
 import styles from '../styles/about-us.module.css';
 import NavBar from '../components/navbar';
 import Footer from '../components/footer';  
